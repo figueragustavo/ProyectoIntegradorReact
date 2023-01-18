d428d00ef514db94c3dc96614a2df91b2f94ff3e
gfiguera <figueragustavo@gmail.com>
2023-01-18 20:59:52 -0300

    se crea estructura del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+//import { AppContext } from "./Store/AppContext";
+
+function App() {
+    //const post = useContext(AppContext);
+
+    return (
+    <div>
+        <h1>Noticias</h1>
+    </div>
+)};
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+    return ( 
+        <h1>Hola Footer</h1>
+    
+    );
+}
+
+export default Footer;
