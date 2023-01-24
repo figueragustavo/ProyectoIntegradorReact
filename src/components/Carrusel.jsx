726221d2e3994ec6e5f4778a5e39de7e2c35f3c0
sharonOliva <olivasharon1819@gmail.com>
2023-01-24 17:48:43 -0300

    imagen inicio

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -6,7 +6,7 @@ function Carrusel() {
  
   return (
     <div className="row mx-0 my-5 gap-4 justify-content-center"  >
-      <img src="https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2020/08/hipertextual-podras-pasar-noche-ultimo-blockbuster-mundo-gracias-airbnb-2020867279.jpg?resize=1536%2C991&ssl=1"width="300" height="800" alt="BLOCKBUSTER">
+      <img src="https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2020/08/hipertextual-podras-pasar-noche-ultimo-blockbuster-mundo-gracias-airbnb-2020867279.jpg?resize=1536%2C991&ssl=1"width="300" height="500" alt="BLOCKBUSTER">
       </img>
    </div>
   )
