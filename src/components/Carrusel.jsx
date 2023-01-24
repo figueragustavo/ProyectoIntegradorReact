453200f990f437acf3e8efe4051f7c1774c6eb65
sharonOliva <olivasharon1819@gmail.com>
2023-01-24 20:54:42 -0300

    imagen inicio

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import blockbuster from '../blockbuster.webp';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -6,7 +7,7 @@ function Carrusel() {
  
   return (
     <div className="row mx-0 my-5 gap-4 justify-content-center"  >
-      <img src="https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2020/08/hipertextual-podras-pasar-noche-ultimo-blockbuster-mundo-gracias-airbnb-2020867279.jpg?resize=1536%2C991&ssl=1"width="300" height="500" alt="BLOCKBUSTER">
+      <img src={blockbuster} width="300" height="500" alt="BLOCKBUSTER">
       </img>
    </div>
   )
